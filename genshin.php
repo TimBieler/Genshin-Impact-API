e89263e9156992fd108effd2661f1357c06a59bf
TimBieler <90199846+TimBieler@users.noreply.github.com>
2021-10-12 16:33:36 +0200

    Add files via upload

--- genshin.php
+++ genshin.php
@@ -1,3 +1,4 @@
+
 <?php
 
 
@@ -9,7 +10,7 @@ $headers = array(
     'Accept: application/json',
     'Content-Type: application/json',
 );
-$url = "https://api.genshin.dev/characters/Zhongli?json";
+$url = "https://api.genshin.dev/characters/Bennett?json";
 
 curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
@@ -31,3 +32,68 @@ echo $array->weapon . PHP_EOL;
 for ($i = 0; $i < count($array->skillTalents); $i++) {
     echo $array->skillTalents[$i]->name . PHP_EOL;
 }
+
+$curl = curl_init();
+
+$headers = array(
+    'Accept: application/json',
+    'Content-Type: application/json',
+);
+
+$url = "https://api.genshin.dev/artifacts?json";
+
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$result = curl_exec($curl);
+curl_close($curl);
+$artifacts = json_decode($result);
+
+for ($set = 0; $set != 30; $set++) {
+    echo ($set . " " . $artifacts[$set]);
+    print("\n");
+}
+
+$input = readline("Eingabe: ");
+
+
+
+//Artifacts Info
+
+$curl = curl_init();
+
+$headers = array(
+    'Accept: application/json',
+    'Content-Type: application/json',
+);
+
+$url = "https://api.genshin.dev/artifacts/$artifacts[$input]?json";
+
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$result = curl_exec($curl);
+
+curl_close($curl);
+
+$arInfo = json_decode($result);
+
+echo $arInfo->name . PHP_EOL;
+echo $arInfo->max_rarity . PHP_EOL;
+$twopiece = "2-piece_bonus";
+$fourpiece = "4-piece_bonus";
+echo $arInfo->$twopiece . PHP_EOL;
+echo $arInfo->$fourpiece . PHP_EOL;
+
+
+/*
+Nummer 10, komisches Leaked Artefakt, keine Einträge vorhanden
+Number 20
+
+*/
