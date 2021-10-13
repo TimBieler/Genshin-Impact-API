e5665f6d1c7dc7d5586c10f49ddd2dfa9a6bf312
LukeMeier <73538499+LukeMeier@users.noreply.github.com>
2021-10-13 17:13:26 +0200

    Add files via upload
    Tages UPDATE

--- genshin.php
+++ genshin.php
@@ -1,5 +1,6 @@
-<?php
 
+<?php
+//Chinesische Republikaner
 function loadData($url)
 {
     $curl = curl_init();
@@ -28,7 +29,8 @@ $array = json_decode($result);
 // print_r($array);
 echo "1 " . $array->types[0] . PHP_EOL;
 echo "2 " . $array->types[1] . PHP_EOL;
-echo "3 " . $array->types[8] . PHP_EOL;
+echo "5 " . $array->types[4] . PHP_EOL;
+echo "9 " . $array->types[8] . PHP_EOL;
 print("\n");
 $i = readline("Eingabe: ");
 switch ($i) {
@@ -37,13 +39,17 @@ switch ($i) {
 
         $artifacts = json_decode($result);
 
-        for ($set = 0; $set != 30; $set++) {
+        for ($set = 0; $set != 37; $set++) {
             echo ($set . " " . $artifacts[$set]);
             print("\n");
         }
 
         $input = readline("Eingabe: ");
 
+        if ($input > 36) {
+            print("Das ist keine Auswahl\n");
+            exit;
+        }
         $result = loadData("https://api.genshin.dev/artifacts/$artifacts[$input]?json");
 
         $arInfo = json_decode($result);
@@ -53,6 +59,11 @@ switch ($i) {
         $two = "2-piece_bonus";
         $four = "4-piece_bonus";
 
+        $arrnam = array("Prayers For Destiny", "Prayers For Illumination", "Prayers For Wisdom", "Prayers For Springtime
+        ", "Prayers For The Firmament\nEin Artefakt, dass in der Beta existierte, heute aber nicht mehr");
+        $arrbon = array("Hydro", "Pyro", "Electro", "Cryo", "Anemo");
+        $arrcou = $input - 20;
+
         if ($input == 10) {
             echo "\n\n";
             echo $arError;
@@ -61,37 +72,12 @@ switch ($i) {
             echo "Maximale Seltenheit : 5\n";
             echo "Bonus bei 2 Stück : unbekannt\n";
             echo "Bonus bei 4 Stück : unbekannt";
-        } elseif ($input == 20) {
+        } elseif ($input > 19 && $input < 25) {
             echo "\n\n";
             echo $arError;
-            echo "\nPrayers For Destiny\n";
+            echo "\n$arrnam[$arrcou]\n";
             echo $rare4;
-            echo "Bonus bei 1 Stück : Affected by Hydro for 40% less time\n";
-        } elseif ($input == 21) {
-            echo "\n\n";
-            echo $arError;
-            echo "\nPrayers For Illumination\n";
-            echo $rare4;
-            echo "Bonus bei 1 Stück : Affected by Pyro for 40% less time\n";
-        } elseif ($input == 22) {
-            echo "\n\n";
-            echo $arError;
-            echo "\nPrayers For Wisdom\n";
-            echo $rare4;
-            echo "Bonus bei 1 Stück : Affected by Electro for 40% less time.\n";
-        } elseif ($input == 23) {
-            echo "\n\n";
-            echo $arError;
-            echo "\nPrayers For Springtime\n";
-            echo $rare4;
-            echo "Bonus bei 1 Stück : Affected by Cryo for 40% less time\n";
-        } elseif ($input == 24) {
-            echo "\n\n";
-            echo $arError;
-            echo "\nPrayers For The Firmament\n";
-            echo "Ein Artefakt, dass in der Beta existierte, heute aber nicht mehr\n";
-            echo $rare4;
-            echo "Bonus bei 1 Stück : Affected by Anemo for 40% less time\n";
+            echo "Bonus bei 1 Stück: Affected by $arrbon[$arrcou] for 40% less time\n";
         } else {
             echo $arInfo->name . PHP_EOL;
             echo "Maximale Seltenheit : ", $arInfo->max_rarity;
@@ -99,6 +85,7 @@ switch ($i) {
             echo "Bonus bei 2 Stück : ", $arInfo->$two;
             echo "\n";
             echo "Bonus bei 4 Stück : ", $arInfo->$four;
+            echo "\n";
         }
         break;
     case 2:
@@ -122,16 +109,92 @@ switch ($i) {
         $info = json_decode($result);
         echo "Name: " . $info->name . PHP_EOL;
         echo "Nation: " . $info->nation . PHP_EOL;
-        echo "Waffe: " . $info->weapon . PHP_EOL;
+        echo "Waffe: " . $info->reactions . PHP_EOL;
         echo "Göttliches Auge: Typ " . $info->vision . PHP_EOL;
         break;
     case 3:
+        break;
+    case 4:
+        break;
+    case 5:
+        //ELEMENTS---------------------------------
+        $result = loadData("https://api.genshin.dev/elements?json");
+
+        $elements = json_decode($result, true);
+        for ($x = 0; $x != 7; $x++) {
+            echo ($x . " " . $elements[$x]);
+            print("\n");
+        }
+
+        $input = readline("\nEingabe: ");
+
+        $result = loadData("https://api.genshin.dev/elements/$elements[$input]?json");
+
+        $effect = json_decode($result);
+
+        echo "\nName: ", $effect->name . PHP_EOL;
+        if ($input == 1 || $input == 3 || $input == 5) {
+            for ($w = 0; $w < 3; $w++){
+                echo "\nEffekt: ", $effect->reactions[$w]->name . PHP_EOL;
+                echo "Reaktion mit: ", $effect->reactions[$w]->elements[0] . PHP_EOL;
+                echo $effect->reactions[$w]->description . PHP_EOL;
+                echo "\n";
+            }
+        } elseif ($input == 6) {
+            for ($w = 0; $w < 4; $w++){
+                echo "Effekt: ", $effect->reactions[$w]->name . PHP_EOL;
+                echo "Reaktion mit: ", $effect->reactions[$w]->elements[0] . PHP_EOL;
+                echo $effect->reactions[$w]->description . PHP_EOL;
+                echo "\n";
+            }
+        } elseif ($input == 0 || $input == 4) {
+            for ($w = 0; $w < 4; $w++){
+                echo "Effekt: ", $effect->reactions[0]->name . PHP_EOL;
+                echo "Reaktion mit: ", $effect->reactions[0]->elements[$w] . PHP_EOL;
+                echo $effect->reactions[0]->description . PHP_EOL;
+                echo "\n";
+            }
+        } elseif ($input == 2) {
+            for ($w = 0; $w < 1; $w++){
+                echo "Effekt: ", $effect->reactions[0]->name . PHP_EOL;
+                echo "Reaktion mit: ", $effect->reactions[0]->elements[$w] . PHP_EOL;
+                echo $effect->reactions[0]->description . PHP_EOL;
+                echo "\n";
+            }
+        } else {
+            print("Das ist keine Auswahl!");
+        }
+        break;
+    case 6:
+        break;
+    case 7:
+        break;
+    case 8:
+        break;
+    case 9:
         $result = loadData("https://api.genshin.dev/weapons?json");
         $weapon = json_decode($result, true);
-        for ($c = 0; $c != 5; $c++) {
+        for ($c = 0; $c != 124; $c++) {
             echo ($c . " " . $weapon[$c]);
             print("\n");
         }
+        print("\n\n");
+        $putwea = readline("Eingabe: ");
+        if ($putwea > -1 && $putwea < 124) {
+            $result = loadData("https://api.genshin.dev/weapons/$weapon[$putwea]?json");
+        } else {
+            print("Keine Mögliche Auswahl\n");
+            exit;
+        }
+        $weapstat = json_decode($result);
+
+        print("\n\n");
+        echo "Name: ", $weapstat->name . "\n";
+        echo "Typ: ", $weapstat->type . "\n";
+        echo "Seltenheit: ", $weapstat->rarity . " Sterne\n";
+        echo "Standard Schaden: ", $weapstat->baseAttack . "\n";
+        echo "Substat: ", $weapstat->subStat . "\n";
+        echo "Fähigkeit: ", $weapstat->passiveDesc . "\n";
         break;
     default:
         print("Das ist keine Auswahl\n");
